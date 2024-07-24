1691c34f55683ae2da5b15dd78ed3510c2ed284b
AngeloCinaWD <angelo.cina@yahoo.it>
2024-07-24 19:56:38 +0200

    varie

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-// setto la porta che voglio per runnare il web server con npm run serve, default è 5173
-vite.config.js => server: {port: 3000}
+setto la porta che voglio per runnare il web server con npm run serve, default è 5173 vite.config.js => server: {port: 3000}
+
+@ nei path è la shorthand per src folder
--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,15 @@
-<!-- COMPOSITION API SHORTER VERSION, non c'è la funzione setup() che wrappa, non c'è l'export default, non c'è il return. Il tag <script> ha l'attributo setup-->
 <script setup>
-import { ref, onMounted } from 'vue';
-
-    const name = ref('John Doe');
-    const status = ref(true);
-    const  tasks = ref(['task one', 'task two', 'task three']);
-    const newTask = ref('');
-
-    const changeUserStatus = () => {
-      status.value = !status.value
-    };
-
-    const addTask = () => {
-      if(newTask.value.trim() !== '') {
-        tasks.value.push(newTask.value);
-        newTask.value = '';
-      }
-    };
-
-    const deleteTask = (index) => {
-        tasks.value.splice(index, 1);
-    };
-
-    onMounted(async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-        const data = await response.json();
-        tasks.value = data.map(task => task.title);
-      } catch (error) {
-        console.log('Error fetching tasks');
-      }
-    });
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status">User is active</p>
-  <p v-else>User is inactive</p>
-
-  <!-- <button v-on:click="changeUserStatus">Change User Status</button> -->
-  <!-- shorthand del v-on:event è @event -->
-  <button @click="changeUserStatus">Change User Status</button>
-  <br>
-  <h3>Tasks:</h3>
-  <br>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="index">
-      <span>{{ task }}</span>
-      <button @click="deleteTask(index)">x</button>
-    </li>
-  </ul>
-  <br>
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task:  </label>
-    <input type="text" name="addTask" id="addTask" v-model="newTask">
-    <button type="submit">Submit</button>
-  </form>
+  <!-- <Navbar></Navbar> -->
+  <Navbar />
+  <!-- <Hero title="Test Title" subtitle="Subtest Title" /> -->
+  <Hero />
+  <HomeCards />
+  <JobListings />
 </template>
-
-<style scoped>
-  h1 {
-    color: red;
-  }
-</style>
