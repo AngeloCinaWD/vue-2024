919b1942ed0e2d428a2dfd01bbd42ad0ab3248a2
unknown <a.cina@procreatech.com>
2024-07-26 14:02:26 +0200

    primeicons, computed properties

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+[video](https://www.youtube.com/watch?v=VeNfHj6MhgA&ab_channel=TraversyMedia)
+
+[tailwind_vue](https://v2.tailwindcss.com/docs/guides/vue-3-vite) 
+
 setto la porta che voglio per runnare il web server con npm run serve, default è 5173 vite.config.js => server: {port: 3000}
 
 @ nei path è la shorthand per src folder
+
+[primeicons_vue](https://primevue.org/icons)
+
+[primeicons](https://github.com/primefaces/primeicons)
+
--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,8 +1,29 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, computed } from 'vue';
 
-defineProps({
+// per accedere ad una props nello script devo salvare le props in una variabile
+// defineProps({
+//   job: Object,
+// });
+
+const props = defineProps({
   job: Object,
+});
+
+const showFullDescription = ref(false);
+
+const toggleShowFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+}
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description;
+
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + ' ...';
+  }
+
+  return description;
 });
 </script>
 
@@ -15,7 +36,9 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <!-- {{ job.description }} -->
+          <div>{{ truncatedDescription }}</div>
+          <button @click="toggleShowFullDescription" class="text-green-500 hover:text-green-600 mb-5"> {{ showFullDescription ? 'Less' : 'More' }} </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }}</h3>
@@ -24,7 +47,7 @@ defineProps({
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
         <a
